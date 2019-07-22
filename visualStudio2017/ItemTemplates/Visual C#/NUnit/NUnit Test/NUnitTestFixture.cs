c47c1fcb6d27a49578cf7d7133fc06f6dfac1adb
Bryan Drenner <1829108+bryandrenner@users.noreply.github.com>
2019-07-22 13:30:59 -0500

    VS2017: item template for NUnit Test

---
+++ visualStudio2017/ItemTemplates/Visual C#/NUnit/NUnit Test/NUnitTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$
+namespace $rootnamespace$
+{
+    [TestFixture]
+    public class $safeitemrootname$
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+    }
+}
